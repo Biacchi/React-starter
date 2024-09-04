103f5bdf6f5781a732a4ee13eea68e4131360cf1
Gabriel Biacchi <gabrielbiacchi.gb@gmail.com>
2024-09-04 13:47:19 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to heeeey.
+        </p>
+        <h1>Hello world aaaaaaaaaaaaaa hey hello</h1>
+        <h1>Not this shit is working!</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
